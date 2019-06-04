b4e6c59ca6745eb8112adee8db855d4870749d0a
Andreas Arrgård <andreas.arrgard@insclear.se>
2019-06-04 13:36:44 +0200

    solidproxy: added logging test

--- SolidProxy.Tests/InterfaceVsClassTests.cs
+++ SolidProxy.Tests/InterfaceVsClassTests.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using SolidProxy.Core.Proxy;
-using SolidProxy.GeneratorCastle;
 
 namespace SolidProxy.Tests
 {
---
+++ SolidProxy.Tests/MicrosoftLoggingTests.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using SolidProxy.Core.Proxy;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace SolidProxy.Tests
+{
+    public class MicrosoftLoggingTests : TestBase
+    {
+        public class LoggingAdvice<TObject, TMethod, TAdvice> : ISolidProxyInvocationAdvice<TObject, TMethod, TAdvice> where TObject : class
+        {
+            public LoggingAdvice(ILogger<LoggingAdvice<TObject, TMethod, TAdvice>> logger)
+            {
+                Logger = logger;
+            }
+            public ILogger Logger { get; }
+
+            public async Task<TAdvice> Handle(Func<Task<TAdvice>> next, ISolidProxyInvocation<TObject, TMethod, TAdvice> invocation)
+            {
+                var methodInfo = invocation.SolidProxyInvocationConfiguration.MethodInfo;
+                try
+                {
+                    Logger.LogTrace($"Entering - {methodInfo.Name}");
+                    return await next();
+                }
+                finally
+                {
+                    Logger.LogTrace($"Exiting - {methodInfo.Name}");
+                }
+            }
+        }
+
+        public interface ITestInterface
+        {
+            void DoSomething();
+        }
+
+        public class LoggerProvider : ILoggerProvider
+        {
+            public class LocalLogger : ILogger
+            {
+                public LocalLogger()
+                {
+                    Logger = new StringBuilder();
+                }
+                public IDisposable BeginScope<TState>(TState state)
+                {
+                    return null;
+                }
+
+                public bool IsEnabled(LogLevel logLevel)
+                {
+                    return true;
+                }
+
+                public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+                {
+                    Logger.AppendLine(formatter(state, exception));
+                }
+                public StringBuilder Logger { get; }
+            }
+
+            public LoggerProvider()
+            {
+                Logger = new LocalLogger();
+            }
+            public ILogger CreateLogger(string categoryName)
+            {
+                return Logger;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public LocalLogger Logger { get; }
+        }
+
+        [Test]
+        public void TestInterfaceInvocation()
+        {
+            var loggerProvider = new LoggerProvider();
+
+            var services = SetupServiceCollection();
+            services.AddLogging(o =>
+            {
+                o.SetMinimumLevel(LogLevel.Trace);
+                o.AddProvider(loggerProvider);
+            });
+            services.AddTransient<ITestInterface>();
+
+            services.GetSolidConfigurationBuilder()
+                .AddAdvice(typeof(LoggingAdvice<,,>), o => o.MethodInfo.DeclaringType == typeof(ITestInterface));
+            var sp = services.BuildServiceProvider();
+            try
+            {
+                sp.GetRequiredService<ITestInterface>().DoSomething();
+            } catch(NotImplementedException) { }
+            Assert.AreEqual("Entering - DoSomething\r\nExiting - DoSomething\r\n", loggerProvider.Logger.Logger.ToString());
+        }
+    }
+}
